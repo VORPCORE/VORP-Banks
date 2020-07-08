26d43eaf58774c65e00c1ac15423ed48b8e66f1c
Avililla <54501703+Avililla@users.noreply.github.com>
2020-07-08 22:29:07 +0200

    Sql baks transactions

--- database.sql
+++ database.sql
@@ -20,3 +20,17 @@ CREATE TABLE `bank_users` (
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
+
+CREATE TABLE `transactions` (
+	`bank` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`fromIdentifier` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`toIdentifier` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`quantity` DOUBLE(22,0) NULL DEFAULT NULL,
+	`date` DATE NULL DEFAULT NULL,
+	INDEX `FK_transactions_banks` (`bank`) USING BTREE,
+	CONSTRAINT `FK_transactions_banks` FOREIGN KEY (`bank`) REFERENCES `vorp`.`banks` (`name`) ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+
