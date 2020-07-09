df943e2ae3e7f4cb33d68c85e002aaf207b2da44
Avililla <54501703+Avililla@users.noreply.github.com>
2020-07-09 04:31:27 +0200

    make good foreign key in order to give sense to it

--- database.sql
+++ database.sql
@@ -15,7 +15,7 @@ CREATE TABLE `bank_users` (
 	`money` DOUBLE(22,0) NULL DEFAULT '0',
 	`gold` DOUBLE(22,0) NULL DEFAULT '0',
 	INDEX `name` (`name`) USING BTREE,
-	CONSTRAINT `bank` FOREIGN KEY (`name`) REFERENCES `vorp`.`banks` (`name`) ON UPDATE RESTRICT ON DELETE CASCADE
+	CONSTRAINT `bank` FOREIGN KEY (`name`) REFERENCES `vorp`.`banks` (`name`) ON UPDATE CASCADE ON DELETE CASCADE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
@@ -28,7 +28,7 @@ CREATE TABLE `transactions` (
 	`quantity` DOUBLE(22,0) NULL DEFAULT NULL,
 	`date` DATE NULL DEFAULT NULL,
 	INDEX `FK_transactions_banks` (`bank`) USING BTREE,
-	CONSTRAINT `FK_transactions_banks` FOREIGN KEY (`bank`) REFERENCES `vorp`.`banks` (`name`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	CONSTRAINT `FK_transactions_banks` FOREIGN KEY (`bank`) REFERENCES `vorp`.`banks` (`name`) ON UPDATE CASCADE ON DELETE CASCADE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
